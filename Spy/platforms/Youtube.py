db9c8739daa9dc757f059a75c8b91e2b514977bf
Bloodlinebots <githubsbots@gmail.com>
2025-05-28 15:43:10 +0530

    Update Youtube.py

--- Spy/platforms/Youtube.py
+++ Spy/platforms/Youtube.py
@@ -9,8 +9,8 @@ import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
-from spy.utils.database import is_on_off
-from spy.utils.formatters import time_to_seconds
+from Spy.utils.database import is_on_off
+from Spy.utils.formatters import time_to_seconds
 import os
 import glob
 import random

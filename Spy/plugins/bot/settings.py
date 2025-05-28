9a6a1726c0806289d8af4854cf849f9288e480f6
Bloodlinebots <githubsbots@gmail.com>
2025-05-28 16:17:19 +0530

    Update settings.py

--- Spy/plugins/bot/settings.py
+++ Spy/plugins/bot/settings.py
@@ -104,12 +104,6 @@ async def gib_repo_callback(_, callback_query):
             "https://telegra.ph/file/b1367262cdfbcd0b2af07.mp4", 
             has_spoiler=True, 
             caption="ʟᴜɴᴅ ʟᴇʟᴇ ᴍᴇʀᴀ ʀᴇᴘᴏ ᴋʏᴀ ᴋᴀʀᴇɢᴀ, ʟᴇɢᴀ ᴋʏᴀ ʙʜᴏsᴀᴅɪᴋᴇ"
-Raat ka sannata, dil mein udaasi,
-Sapne to the bade, par kismat thi khaali,
-Kitaabon mein chhupi thi, ek kahani purani,
-Har page par tha dard, jo thi purani yahani ,
-Barosa tha khud par, par kabhi na tha saath,
-Mehnat thi lekin, manzil thi ek raaz ."
         ),
         reply_markup=InlineKeyboardMarkup(
             [

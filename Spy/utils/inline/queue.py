3964fb1dbbf3ed1171e34962bbc583e768d21713
Bloodlinebots <githubsbots@gmail.com>
2025-07-13 01:01:58 +0530

    Update queue.py

--- Spy/utils/inline/queue.py
+++ Spy/utils/inline/queue.py
@@ -66,19 +66,14 @@ def queue_back_markup(_, CPLAY):
 
 def aq_markup(_, chat_id):
     buttons = [
-     #   [
-            #InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            #InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            #InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            #InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        #],
         [
             InlineKeyboardButton(
                 text=_["S_B_9"], url=SUPPORT_CHAT,
             ),
             InlineKeyboardButton(
-                    text=_["CLOSE_BUTTON"],
-                    callback_data="close",
+                text=_["CLOSE_BUTTON"],
+                callback_data="close",
             ),
         ]
-    return buttons
+    ]
+    return InlineKeyboardMarkup(buttons)

135a0b4add5a6a696cba41473ce848238f62f9ae
Bloodlinebots <githubsbots@gmail.com>
2025-07-13 08:08:59 +0530

    Update settings.py

--- Spy/plugins/bot/settings.py
+++ Spy/plugins/bot/settings.py
@@ -101,9 +101,9 @@ async def settings_back_markup(client, CallbackQuery: CallbackQuery, _):
 async def gib_repo_callback(_, callback_query):
     await callback_query.edit_message_media(
         media=InputMediaVideo(
-            "https://telegra.ph/file/b1367262cdfbcd0b2af07.mp4", 
+            "https://files.catbox.moe/viv5kw.mp4", 
             has_spoiler=True, 
-            caption="ʟᴜɴᴅ ʟᴇʟᴇ ᴍᴇʀᴀ ʀᴇᴘᴏ ᴋʏᴀ ᴋᴀʀᴇɢᴀ, ʟᴇɢᴀ ᴋʏᴀ ʙʜᴏsᴀᴅɪᴋᴇ"
+            caption="ʙʜᴏsᴀᴅɪᴋᴇ"
         ),
         reply_markup=InlineKeyboardMarkup(
             [
